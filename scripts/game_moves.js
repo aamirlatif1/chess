c7f2ba177be51fe88eceb9eddb9ac5e290c7bd94
Aamir Latif <aamirlatif@gmail.com>
2025-06-28 10:56:43 +0200

    add move validation

--- scripts/game_moves.js
+++ scripts/game_moves.js
@@ -88,17 +88,5 @@ function gameMoves(evt) {
   }
 }
 
-function isValidMove(board, from, to) {
-  console.log(from, to);
-  const pieceToMove = board.square(from.x, from.y).piece;
-  const destPiece = board.square(to.x, to.y).piece;
-  if(destPiece && destPiece.color == pieceToMove.color) {
-    return false;
-  } else {
-    return true;
-  }
-}
 
-
-// piece boundry check.
 // basic move validation.
--- scripts/move_validation.js
+++ scripts/move_validation.js
@@ -1,1 +1,11 @@
 
+function isValidMove(board, from, to) {
+  console.log(from, to);
+  const pieceToMove = board.square(from.x, from.y).piece;
+  const destPiece = board.square(to.x, to.y).piece;
+  if(destPiece && destPiece.color == pieceToMove.color) {
+    return false;
+  } else {
+    return true;
+  }
+}
